214076768ad02e2c6c654a1ec6da32ae83afd855
Ahmet Bozaci <ahmt9417@gmail.com>
2020-10-21 16:45:14 +0300

    created my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -10,5 +10,15 @@ module Enumerable
       return "#<Enumerator: #{self}:my_each>"
     end
   end
+  
+  #my_each_with_index
+  def my_each_with_index(&block)
+    if block_given?
+      for i in self do
+        yield i, self.index(i)
+      end
+    else
+      return "#<Enumerator: #{self}:my_each_with_index>"
+  end
 
 end
