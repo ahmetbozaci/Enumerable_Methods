f0f9aae6596964d1dffd3a42ef62dbb51cbccd46
Ahmet Bozaci <ahmt9417@gmail.com>
2020-10-30 11:59:08 +0300

    refactor my_any,

--- enumerables.rb
+++ enumerables.rb
@@ -45,12 +45,11 @@ module Enumerable
   end
 
   # my_any
+
   def my_any?(arg = nil)
     if block_given?
       my_each do |i|
-        if yield(i)
-          true
-        end
+        return true if yield(i)
       end
       false
 
@@ -62,21 +61,14 @@ module Enumerable
     elsif size.positive? && arg.nil?
       nil_count = 0
       my_each do |i|
-        if i.nil? || i == false
-          nil_count += 1
-        end
+        nil_count += 1 if i.nil? || i == false
       end
-      if nil_count == size
-        false
-      else
-        true
-      end
+      nil_count != size # if all elements are nil or false "return false" otherwise "return true"
+
     # if class
     elsif arg.class == Class
       my_each do |i|
-        if i.is_a?(arg)
-          true
-        end
+        return true if i.is_a?(arg)
       end
       false
 
@@ -84,9 +76,7 @@ module Enumerable
     elsif arg.class == Regexp
       my_each do |i|
         index = i =~ arg
-        if index.class == Integer
-          true
-        end
+        return true if index.class == Integer
       end
       false
     end
@@ -303,4 +293,65 @@ end
 
 multiply_els([1, 3, 7, 9])
 
+# EXAMPLES
+arr = [32, 10, 21, 4, 5]
 
+# my_each
+arr.my_each do |n|
+  n += 1
+  print "#{n}-"
+end
+
+# my_each_with_index
+arr.my_each_with_index do |i, n|
+  print "#{i}:#{n} - "
+end
+
+# my_map
+arr.my_map do |i|
+  i += 1
+  print "#{i} "
+end
+
+my_proc = proc { |i| i * 2 }
+arr.my_map(&my_proc)
+
+# my_select
+arr.my_select(&:even?) #=> [32, 10, 4]
+
+# my_any
+%w[orange banane apple].my_any? { |word| word.length >= 3 } #=> true
+%w[orange banane apple].my_any? { |word| word.length >= 10 } #=> false
+%w[orange banane apple].my_any?(/d/) #=> false
+[3, 'apple', 1].my_any?(String) #=> true
+[nil, nil, false].my_any? #=> false
+[].my_any? #=> false
+
+# my_all
+%w[orange banana apple].my_all? { |word| word.length >= 3 } #=> true
+%w[orange banana apple].my_all? { |word| word.length >= 4 } #=> false
+%w[orange banana apple].my_all?(/t/) #=> false
+[1, 2i, 3.14].my_all?(Numeric) #=> true
+[nil, true, 99].my_all? #=> false
+[].my_all? #=> true
+
+# my_none
+%w[orange banane apple].my_none? { |word| word.length == 5 } #=> false
+%w[orange banane apple].my_none? { |word| word.length <= 4 } #=> true
+%w[orange banane apple].my_none?(/d/) #=> true
+[1, 314, 4.2].my_none?(Float) #=> false
+[].my_none? #=> true
+[nil].my_none? #=> true
+[nil, false].my_none? #=> true
+[nil, false, true].my_none? #=> false
+
+# my_count
+ary = [4, 5, 2, 2, 6, 7, 2, 2, 3]
+ary.my_count
+ary.my_count(2)
+
+# my_inject
+(5..10).my_inject(5) { |sum, n| sum + n }
+(5..50).my_inject { |prod, n| prod * n }
+array = [1, 2, 3, 4, 8]
+array.my_inject(:+)
