7fcd65ba54dfb6d0e740c8df6cee798ffe04e722
Your Name <basaijapascal9@gmail.com>
2020-12-17 13:45:52 +0300

    add tests for my_each_with index and my_select

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -4,7 +4,7 @@ describe Enumerable do
   arr = [32, 10, 21, 4, 5]
   range = (4..10)
   test_block = proc { |n| n += 1 }
-  
+  test_block2 = proc {|i, n|print "#{i}:#{n} - "}
  
   describe '#my_each' do
     it 'my_each output has to same with each' do
@@ -16,6 +16,22 @@ describe Enumerable do
     end
   
   end
+
+  describe '#my_each_with_index' do
+    it 'iterates over a collection elements with index'do
+      expect(arr.my_each_with_index(&test_block2)).to eq(arr.each_with_index(&test_block2))
+   end 
+  end
+
+  it 'tets my_each_with_index on range' do
+    expect(range.my_each_with_index(&test_block2)).to eq(range.each_with_index(&test_block2))
+    end
+
+    describe '#my_select' do
+      it 'tests my_select for a given collection' do
+        expect(arr.my_select(&:even?)).to eq(arr.select(&:even?))
+  
+    end
 end
 
 # [1,2,3].my_each(&proc{|x| x>2}) == [1,2,3]
